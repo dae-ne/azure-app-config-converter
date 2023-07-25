0696fa18a96b2c8ab67973158e71a356c438d6bb
dae-ne <rafalcz29@gmail.com>
2023-07-25 15:58:41 +0200

    Format code

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,9 @@
 export const handle = (data, options) => {
   const { indentation } = options;
+
   const json = JSON.parse(data);
   const result = json.reduce((p, c) => ({ ...p, [c.name]: c.value }), {});
+
   return JSON
     .stringify(result, null, parseInt(indentation))
     .trim();
