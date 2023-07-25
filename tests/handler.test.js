3dde849bba99c534c54a4ac382686e9b5d24da1c
dae-ne <rafalcz29@gmail.com>
2023-07-25 15:51:37 +0200

    Update unit tests

--- tests/handler.test.js
+++ tests/handler.test.js
@@ -1,10 +1,30 @@
 import { readFileSync } from 'fs';
 import { handle } from '../src/handler';
 
-test('handler should return a correct value', () => {
-  const data = readFileSync('tests/json/input.json').toString();
-  const expected = readFileSync('tests/json/output.json').toString();
-  const options = { indentation: 2 };
+const loadData = () => {
+  const data = readFileSync('tests/json/input.json')
+    .toString()
+    .trim();
+  const expected = readFileSync('tests/json/output.json')
+    .toString()
+    .trim();
+  return { data, expected };
+};
+
+const setOptions = (indentation = 2) => ({
+  indentation
+})
+
+test('handler should return a correct configuration', () => {
+  const { data, expected } = loadData();
+  const options = setOptions();
   const result = handle(data, options);
-  expect(result.trim()).toEqual(expected.trim());
+  expect(result).toEqual(expected);
+});
+
+test('handler should return configuration with different indentation than expected', () => {
+  const { data, expected } = loadData();
+  const options = setOptions(4);
+  const result = handle(data, options);
+  expect(result).not.toEqual(expected);
 });
